5c382a9f70f9e47efbf11ee669bf6a056a155479
Eddie Nuñez <eddie@programadorwp.com>
2018-03-09 10:10:54 -0500

    5.4.7

--- js_composer.php
+++ js_composer.php
@@ -3,7 +3,7 @@
 Plugin Name: WPBakery Page Builder
 Plugin URI: http://wpbakery.com
 Description: Drag and drop page builder for WordPress. Take full control over your WordPress site, build any layout you can imagine – no programming knowledge required.
-Version: 5.4.5
+Version: 5.4.7
 Author: Michael M - WPBakery.com
 Author URI: http://wpbakery.com
 GitHub Plugin URI: https://github.com/eddiewp/js_composer
@@ -20,7 +20,7 @@ if ( ! defined( 'WPB_VC_VERSION' ) ) {
 	/**
 	 *
 	 */
-	define( 'WPB_VC_VERSION', '5.4.5' );
+	define( 'WPB_VC_VERSION', '5.4.7' );
 }
 
 /**
